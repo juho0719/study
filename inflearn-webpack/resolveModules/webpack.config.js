091f51ead6cc1dfc09e6e413ea8e61f8da1b6719
juho0719 <juho0719@naver.com>
2019-01-22 11:02:23 +0900

    복구중 나온 파일들

--- inflearn-webpack/resolveModules/webpack.config.js
+++ inflearn-webpack/resolveModules/webpack.config.js
@@ -2,10 +2,10 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  mode: 'development',
-  entry: './app/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    mode: 'development',
+    entry: './app/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
