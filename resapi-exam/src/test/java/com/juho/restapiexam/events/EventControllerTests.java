93842ed9639c558deffaaae1393881a85bc63023
juho0719 <juho0719@naver.com>
2019-01-31 14:22:06 +0900

    - Updated a test method for jsonpath(id)

--- resapi-exam/src/test/java/com/juho/restapiexam/events/EventControllerTests.java
+++ resapi-exam/src/test/java/com/juho/restapiexam/events/EventControllerTests.java
@@ -9,7 +9,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -21,10 +25,30 @@ public class EventControllerTests {
 
     @Test
     public void createEvent() throws Exception {
+        // given
+        Event event = Event.builder()
+                .name("spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019, 1, 31, 13, 50, 0))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019, 1, 31, 23, 50, 0))
+                .beginEventDateTime(LocalDateTime.of(2019, 2, 1, 9, 0, 0))
+                .endEventDateTime(LocalDateTime.of(2019, 2, 1, 12, 0, 0))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("삼성역 2번출구")
+                .build();
+
+        // when
+
+        // then
         mockMvc.perform(post("/api/events/")
                     .contentType(MediaType.APPLICATION_JSON_UTF8)
                     .accept(MediaTypes.HAL_JSON))
-                .andExpect(status().isCreated());
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+        ;
     }
 
 }
