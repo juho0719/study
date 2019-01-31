54f73a4ba0f0d974dceb9efbf182f982ea6a3f73
juho0719 <juho0719@naver.com>
2019-01-31 09:54:19 +0900

    - Wrote a test method for 201 status

---
+++ resapi-exam/src/main/java/com/juho/restapiexam/RestapiExamApplication.java
@@ -0,0 +1,14 @@
+package com.juho.restapiexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiExamApplication.class, args);
+    }
+
+}
+
