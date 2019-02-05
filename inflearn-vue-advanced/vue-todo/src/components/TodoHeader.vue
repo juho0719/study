f9576a07e9faa94afbda38cd678de6d042713dcd
Juho Kim <juho0719@naver.com>
2019-02-05 23:41:43 +0900

    todo header 컴포넌트 구현

--- inflearn-vue-advanced/vue-todo/src/components/TodoHeader.vue
+++ inflearn-vue-advanced/vue-todo/src/components/TodoHeader.vue
@@ -1,15 +1,14 @@
 <template>
-    <div>
-        header
-    </div>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
 </template>
 
-<script>
-export default {
-
+<!-- scoped옵션은 부모 컴포넌트를 상속받지 않고 해당 컴포넌트에서만 사용됨-->
+<style scoped>
+h1 {
+    color: #2f3b52;
+    font-weight: 900;           /* 폰트 굴기 */
+    margin: 2.5rem 0 1.5rem;    /* rem : 폰트 크기에 따라 상대적 크기 */
 }
-</script>
-
-<style>
-
 </style>
