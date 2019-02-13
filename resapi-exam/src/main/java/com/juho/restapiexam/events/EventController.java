cf6e823cc73081bb758f21ea5ca234ca235b5a2e
juho0719 <juho0719@naver.com>
2019-02-13 12:32:26 +0900

    Parameter test using JUnitParams

--- resapi-exam/src/main/java/com/juho/restapiexam/events/EventController.java
+++ resapi-exam/src/main/java/com/juho/restapiexam/events/EventController.java
@@ -38,6 +38,7 @@ public class EventController {
         }
 
         Event event = modelMapper.map(eventDto, Event.class);
+        event.update();
         Event newEvent = eventRepository.save(event);
         URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
         return ResponseEntity.created(createUri).body(event);
--- resapi-exam/src/main/java/com/juho/restapiexam/events/EventRepository.java
+++ resapi-exam/src/main/java/com/juho/restapiexam/events/EventRepository.java
@@ -1,7 +1,6 @@
 package com.juho.restapiexam.events;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
 }
