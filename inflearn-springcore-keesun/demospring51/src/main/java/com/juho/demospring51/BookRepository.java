a4b3e2d2220337f048061f6e1b5c836828dfdd41
juho0719 <juho0719@naver.com>
2019-02-17 22:59:22 +0900

    autowired and componentscan and bean's scope studied.

---
+++ inflearn-springcore-keesun/demospring51/src/main/java/com/juho/demospring51/BookRepository.java
@@ -0,0 +1,6 @@
+package com.juho.demospring51;
+
+import org.springframework.stereotype.Repository;
+
+public interface BookRepository {
+}
---
+++ inflearn-springcore-keesun/demospring51/src/main/java/com/juho/demospring51/JuhoBookRepository.java
@@ -0,0 +1,8 @@
+package com.juho.demospring51;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JuhoBookRepository implements BookRepository{
+}
