2870a4b28e6b49943487f3c71feabae9976cf001
juho0719 <juho0719@naver.com>
2019-03-19 00:43:39 +0900

    insta 클론 코딩 시작

---
+++ insta-clone/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import MainScreen from './components/MainScreen';
+
+const AppStackNavigator = createStackNavigator ({
+  Main: {
+    screen: MainScreen    // MainScreen 컴포넌트 네비게이터에 등록
+  }
+});
+
+export default createAppContainer(AppStackNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
