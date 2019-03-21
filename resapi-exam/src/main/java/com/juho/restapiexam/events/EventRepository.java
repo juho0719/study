4f6bec5da98212f9e4d34a70a62656635fb642aa
Juho Kim <juho0719@naver.com>
2019-03-21 09:27:24 +0900

    rest-api-exam deleted

--- resapi-exam/src/main/java/com/juho/restapiexam/events/EventRepository.java
+++
@@ -1,6 +0,0 @@
-package com.juho.restapiexam.events;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Integer> {
-}
--- resapi-exam/src/main/java/com/juho/restapiexam/events/EventStatus.java
+++
@@ -1,6 +0,0 @@
-package com.juho.restapiexam.events;
-
-public enum EventStatus {
-
-    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
-}
