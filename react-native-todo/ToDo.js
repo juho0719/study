85514a40f93cd8b4ae1219601e9e2953f4bfe33c
juho0719 <juho0719@naver.com>
2019-03-22 23:02:21 +0900

    coding ui part 1

---
+++ react-native-todo/ToDo.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { 
+    View, 
+    Text, 
+    TouchableOpacity, 
+    StyleSheet 
+} from "react-native"
+class ToDo extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello Juho</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({});
