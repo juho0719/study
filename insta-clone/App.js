cd4c43803ef1c83b019b5ea66149ba1dcc8cf83e
juho0719 <juho0719@naver.com>
2019-04-09 23:45:10 +0900

    insta navigator added

--- insta-clone/App.js
+++ insta-clone/App.js
@@ -11,21 +11,3 @@ const AppStackNavigator = createStackNavigator ({
 
 export default createAppContainer(AppStackNavigator);
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- insta-clone/components/MainScreen.js
+++ insta-clone/components/MainScreen.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { StyleSheet, Platform } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { Icon } from 'native-base';
 
-export default class App extends Component {
+export default class MainScreen extends Component {
 	// navigationOptions 코드 추가
 	static navigationOptions = {
 		headerLeft: <Icon name='ios-camera' style={{ paddingLeft: 10 }} />,
