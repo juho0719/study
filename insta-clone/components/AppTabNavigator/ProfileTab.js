61ccc6752c90c73fae92c8c8532701cbcbe17448
juho0719 <juho0719@naver.com>
2019-04-11 00:08:56 +0900

    bottom navigator added

--- insta-clone/components/AppTabNavigator/ProfileTab.js
+++ insta-clone/components/AppTabNavigator/ProfileTab.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Icon } from 'native-base';
 
 export default class ProfileTab extends Component {
+	static navigationOptions = {
+		tabBarIcon: ({ tintColor }) => (
+			<Icon name='ios-person' style={{ color: tintColor }} />
+		)
+	}
+
 	render() {
 		return (
 			<View style={style.container}>
