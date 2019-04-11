19916bae9f0cd77f1fb4394c966e9c0f81075129
juho0719 <juho0719@naver.com>
2019-04-12 00:21:30 +0900

    card component added

---
+++ insta-clone/components/CardComponent.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
+import { Card, CardItem, Thumbnail, Body, Left, Right, Button, Icon } from 'native-base';
+
+export default class CardComponent extends Component {
+	render() {
+		return (
+			<Card>
+				<CardItem>
+					<Left>
+						<Thumbnail source={{ uri: 'https://steemitimages.com/u/anpigon/avatar'}} />
+						<Body>
+							<Text>Anpigon</Text>
+							<Text note>Apr 12, 2019</Text>
+						</Body>
+					</Left>
+				</CardItem>
+				<CardItem cardBody>
+					<Image
+						source={{ uri: 'https://user-images.githubusercontent.com/3969643/51441420-b41f1c80-1d14-11e9-9f5d-af5cd3a6aaae.png'}}	
+						style={{ height:200, width: null, flex: 1}} />
+				</CardItem>
+				<CardItem style={{ height: 45}}>
+					<Left>
+						<Button transparent>
+							<Icon name='ios-heart' style={{ color: 'black' }} />
+						</Button>
+						<Button transparent>
+							<Icon name='ios-chatbubbles' style={{ color: 'black' }} />
+						</Button>
+						<Button transparent>
+							<Icon name='ios-send' style={{ color: 'black' }} />
+						</Button>
+					</Left>
+				</CardItem>
+				<CardItem>
+					<Text>
+						<Text style={{ fontWeight: '900' }}>Anpigon</Text>
+						이번에는 리액트 네이티브(React Native)로 인스타그램 UI를 구현하는 포스팅
+						</Text>
+				</CardItem>
+				<CardItem>
+					<View style={style.container}>
+						<Text>CardComponent</Text>
+					</View>
+				</CardItem>
+					
+			</Card>
+		);
+	}
+}
+
+const style = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+});
