f4788b1160a2c617f9ce10cf18ff9c50f2a12222
juho0719 <juho0719@naver.com>
2019-04-11 23:16:46 +0900

    swipe tab added

--- insta-clone/components/MainScreen.js
+++ insta-clone/components/MainScreen.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 import { Icon } from 'native-base';
 import HomeTab from './AppTabNavigator/HomeTab';
 import SearchTab from './AppTabNavigator/SearchTab';
 import AddMediaTab from './AppTabNavigator/AddMediaTab';
 import LikesTab from './AppTabNavigator/LikesTab';
 import ProfileTab from './AppTabNavigator/ProfileTab';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { createMaterialTopTabNavigator, createAppContainer } from 'react-navigation';
 
 // 하단 탭 네비게이터 생성
-const AppTabNavigator = createBottomTabNavigator({
+const AppTabNavigator = createMaterialTopTabNavigator({
 	HomeTab: { screen: HomeTab },
 	SearchTab: { screen: SearchTab },
 	AddMediaTab: { screen: AddMediaTab },
 	LikesTab: { screen: LikesTab },
 	ProfileTab: { screen: ProfileTab },
+}, {
+	animationEnabled: true,
+	swipeEnabled: true,
+	tabBarPosition: "bottom",
+	tabBarOptions: {
+		style: {
+			...Platform.select({
+				ios: {
+					backgroundColor: 'white'
+				}
+			})
+		},
+		iconStyle: { height: 100 },
+		activeTintColor: '#000',
+		inactiveTintColor: '#d1cece',
+		upperCaseLabel: false,
+		showLabel: false,
+		showIcon: true
+	}
 });
 
 const AppTabContainer = createAppContainer(AppTabNavigator);
