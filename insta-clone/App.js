33f0863717ea6eb562109c53339434f66e2c6938
juho0719 <juho0719@naver.com>
2019-04-17 23:05:02 +0900

    react-navigator test

--- insta-clone/App.js
+++ insta-clone/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import MainScreen from './components/MainScreen';
+import HomeTab from './AppTabNavigator/HomeTab';
+import SearchTab from './AppTabNavigator/SearchTab';
+import AddMediaTab from './AppTabNavigator/AddMediaTab';
+import LikesTab from './AppTabNavigator/LikesTab';
+import ProfileTab from './AppTabNavigator/ProfileTab';
 
 const AppStackNavigator = createStackNavigator ({
   Main: {
     screen: MainScreen    // MainScreen 컴포넌트 네비게이터에 등록
-  }
+  },
+  BottomNavigator: AppTabNavigator
+});
+
+// 하단 탭 네비게이터 생성
+const AppTabNavigator = createMaterialTopTabNavigator({
+	HomeTab: { screen: HomeTab },
+	SearchTab: { screen: SearchTab },
+	AddMediaTab: { screen: AddMediaTab },
+	LikesTab: { screen: LikesTab },
+	ProfileTab: { screen: ProfileTab },
+}, {
+	animationEnabled: true,
+	swipeEnabled: true,
+	tabBarPosition: "bottom",
+	tabBarOptions: {
+		style: {
+			...Platform.select({
+				ios: {
+					backgroundColor: 'white'
+				}
+			})
+		},
+		iconStyle: { height: 100 },
+		activeTintColor: '#000',
+		inactiveTintColor: '#d1cece',
+		upperCaseLabel: false,
+		showLabel: false,
+		showIcon: true
+	}
 });
 
 export default createAppContainer(AppStackNavigator);
--- insta-clone/components/MainScreen.js
+++ insta-clone/components/MainScreen.js
@@ -1,42 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Platform } from 'react-native';
 import { Icon } from 'native-base';
-import HomeTab from './AppTabNavigator/HomeTab';
-import SearchTab from './AppTabNavigator/SearchTab';
-import AddMediaTab from './AppTabNavigator/AddMediaTab';
-import LikesTab from './AppTabNavigator/LikesTab';
-import ProfileTab from './AppTabNavigator/ProfileTab';
-import { createMaterialTopTabNavigator, createAppContainer } from 'react-navigation';
-
-// 하단 탭 네비게이터 생성
-const AppTabNavigator = createMaterialTopTabNavigator({
-	HomeTab: { screen: HomeTab },
-	SearchTab: { screen: SearchTab },
-	AddMediaTab: { screen: AddMediaTab },
-	LikesTab: { screen: LikesTab },
-	ProfileTab: { screen: ProfileTab },
-}, {
-	animationEnabled: true,
-	swipeEnabled: true,
-	tabBarPosition: "bottom",
-	tabBarOptions: {
-		style: {
-			...Platform.select({
-				ios: {
-					backgroundColor: 'white'
-				}
-			})
-		},
-		iconStyle: { height: 100 },
-		activeTintColor: '#000',
-		inactiveTintColor: '#d1cece',
-		upperCaseLabel: false,
-		showLabel: false,
-		showIcon: true
-	}
-});
-
-const AppTabContainer = createAppContainer(AppTabNavigator);
 
 export default class MainScreen extends Component {
 	// navigationOptions 코드 추가
@@ -47,6 +11,7 @@ export default class MainScreen extends Component {
 	}
 
 	render() {
+		const { AppTabContainer } = this.props;
 		return (
 			<AppTabContainer />
 		);
