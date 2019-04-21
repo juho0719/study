47e6191075c7028993a9b83af4aaafcc34fdef76
juho0719 <juho0719@naver.com>
2019-04-21 23:14:01 +0900

    createstacknavigator added

--- insta-clone/App.js
+++ insta-clone/App.js
@@ -8,48 +8,53 @@ import AddMediaTab from './components/AppTabNavigator/AddMediaTab';
 import LikesTab from './components/AppTabNavigator/LikesTab';
 import ProfileTab from './components/AppTabNavigator/ProfileTab';
 
-
 // 하단 탭 네비게이터 생성
-const AppTabNavigator = createMaterialTopTabNavigator({
-	HomeTab: { 
-    screen: HomeTab,
-    navigationOptions: {
-      headerLeft: <Icon name='ios-camera' style={{ paddingLeft: 10 }} />,
-      title: 'Instagram',
-      headerRight: <Icon name='ios-send' style={{ paddingRight: 10 }} />
-    } 
-  },
-	SearchTab: { 
-    screen: SearchTab,
-    navigationOptions: {
-      headerLeft: <Icon name='ios-camera' style={{ paddingLeft: 10 }} />,
-      title: 'Instagram',
-      headerRight: <Icon name='ios-send' style={{ paddingRight: 10 }} />
+const AppTabNavigator = createMaterialTopTabNavigator(
+  {
+    HomeTab: createStackNavigator({ 
+      HomeTab: {
+        screen: HomeTab,
+        navigationOptions: {
+          headerLeft: <Icon name='ios-camera' style={{ paddingLeft: 10 }} />,
+          title: 'Instagram',
+          headerRight: <Icon name='ios-send' style={{ paddingRight: 10 }} />
+        }
+      }
+    }),
+    SearchTab: { screen: SearchTab },
+    AddMediaTab: { screen: AddMediaTab },
+    LikesTab: { screen: LikesTab },
+    ProfileTab: { screen: ProfileTab },
+  }, 
+  {
+    animationEnabled: true,
+    swipeEnabled: true,
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      style: {
+        ...Platform.select({
+          ios: {
+            backgroundColor: 'white'
+          }
+        })
+      },
+      iconStyle: { height: 100 },
+      activeTintColor: '#000',
+      inactiveTintColor: '#d1cece',
+      upperCaseLabel: false,
+      showLabel: false,
+      showIcon: true
     }
-  },
-	AddMediaTab: { screen: AddMediaTab },
-	LikesTab: { screen: LikesTab },
-	ProfileTab: { screen: ProfileTab },
-}, {
-	animationEnabled: true,
-	swipeEnabled: true,
-	tabBarPosition: "bottom",
-	tabBarOptions: {
-		style: {
-			...Platform.select({
-				ios: {
-					backgroundColor: 'white'
-				}
-			})
-		},
-		iconStyle: { height: 100 },
-		activeTintColor: '#000',
-		inactiveTintColor: '#d1cece',
-		upperCaseLabel: false,
-		showLabel: false,
-		showIcon: true
-	}
-});
+  }
+);
+
+AppTabNavigator.navigationOptions = ({ navigation }) => {
+  return {
+    headerLeft: <Icon name='ios-camera' style={{ paddingLeft: 10 }} />,
+		title: 'Instagram',
+		headerRight: <Icon name='ios-send' style={{ paddingRight: 10 }} />
+  }
+}
 
 export default createAppContainer(AppTabNavigator);
 
