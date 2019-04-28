052662bcf52d202c653dfeebe5383ad81b5e8cbf
juho0719 <juho0719@naver.com>
2019-04-29 00:00:10 +0900

    react-in-action studied

---
+++ react-in-action/chap02/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const node = document.getElementById("root");
+
+class Post extends Component {
+  render() {
+    return React.createElement("div", { className: "post" }, 
+      React.createElement("h2", { className: "postAuthor", id: this.props.id },
+        this.props.user,
+          React.createElement("span", { className: "postBody" },
+            this.props.content
+          )
+      )
+    );
+  }
+}
+
+Post.PropTypes = {
+  user: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired
+}
+
+const App = React.createElement(Post, {
+  id: 1, content: " said: This is a post", user: "Kim"
+})
+
+ReactDOM.render(App, node);
