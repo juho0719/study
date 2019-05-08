fbafe6d295f24c656dca83d7adac0687b2055a13
juho0719 <juho0719@naver.com>
2019-05-09 01:18:17 +0900

    modified

--- react-in-action/chap04/src/App.js
+++ react-in-action/chap04/src/App.js
@@ -19,6 +19,34 @@ class App extends Component {
       posts: [],
       endpoint: '${process.env.ENDPOINT}/posts?_page=1&_sort=date&_order=DESC&_empbed=comment&_expand=user&_empbed=likes'
     };
+    this.getPosts = this.getPosts.bind(this);
+  }
+  componentDidMount() {
+    this.getPosts();
+  }
+  componentDidCatch(err, info) {
+    console.error(err);
+    console.error(info);
+    this.setState(() => ({
+      error: err
+    }));
+  }
+  getPosts() {
+    API.fetchPosts(this.state.endpoint)
+      .then(res => {
+        return res
+          .json()
+          .then(posts => {
+            const links = parseLinkHeader(res.headers.get('Link'));
+            this.setState(() => ({
+              posts: orderBy(this.state.posts.concat(posts), 'date', 'desc'),
+              endpoint: links.next.url
+            }));
+          })
+          .catch(err => {
+            this.setState(() => ({ error: err }));
+          })
+      });
   }
   static PropTypes = {
     children: PropTypes.node
@@ -36,7 +64,7 @@ class App extends Component {
             <Welcome />
           </div>
           <div>
-            <button className="block">
+            <button className="block" onClick={this.getPosts}>
               Load more posts
             </button>
           </div>
@@ -45,7 +73,7 @@ class App extends Component {
                 imageUrl="/static/assets/ads/orly.png"
             />
           </div>
-        )}
+         )}
       </div>
     );
   }
