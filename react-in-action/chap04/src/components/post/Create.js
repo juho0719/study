d68b3c3b23af29f94640b8e2d8afc893e12a6a90
juho0719 <juho0719@naver.com>
2019-05-11 23:29:56 +0900

    Create post class added

---
+++ react-in-action/chap04/src/components/post/Create.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class CreatePost extends Component {
+	static propTypes = {
+
+	}
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div className="create-post">
+				Create a post - coming (very) soon
+			</div>
+		);
+	}
+}
