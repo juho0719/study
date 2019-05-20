b55aba25af195c77814b81cc35342d28a77650b8
Juho Kim <juho0719@naver.com>
2019-05-20 17:15:35 +0900

    fontawesome test

--- insta-web-clone/src/components/Header/Header.js
+++ insta-web-clone/src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import "./Header.css";
 
 class Header extends React.Component {
 	render() {
 		return (
-			<FontAwesomeIcon icon={['fab', 'apple']} />
+			<FontAwesomeIcon icon={faInstagram} />
 		);
 	}
 }
