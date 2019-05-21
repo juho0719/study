c8463a8662c0491ca1dc5931469f7f89d40a60e0
Juho Kim <juho0719@naver.com>
2019-05-21 17:34:53 +0900

    styled component test

--- insta-web-clone/src/components/Header/Header.js
+++ insta-web-clone/src/components/Header/Header.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import "./Header.css";
+import styled from 'styled-components';
 
 class Header extends React.Component {
 	render() {
 		return (
-			<FontAwesomeIcon icon={faInstagram} size="2x" />
+			<HeaderDiv>
+				<FontAwesomeIcon icon={faInstagram} size="2x" />
+			</HeaderDiv>
 		);
 	}
 }
+
+const HeaderDiv = styled.div`
+	height: 10vh;
+	width: 100%;
+	background-color: pink;
+	align: center;
+`;
 
 export default Header;
