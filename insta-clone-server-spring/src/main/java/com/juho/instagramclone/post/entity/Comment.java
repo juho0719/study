c5525a8650bfca7d133a0319e9785ab4c4e033df
juho0719 <juho0719@naver.com>
2019-07-03 23:37:12 +0900

    - post and comment entity added

---
+++ insta-clone-server-spring/src/main/java/com/juho/instagramclone/post/entity/Comment.java
@@ -0,0 +1,6 @@
+package com.juho.instagramclone.post.entity;
+
+public class Comment {
+    String writer;
+    String content;
+}
---
+++ insta-clone-server-spring/src/main/java/com/juho/instagramclone/post/entity/Post.java
@@ -0,0 +1,12 @@
+package com.juho.instagramclone.post.entity;
+
+import java.util.List;
+
+public class Post {
+
+    String writer;
+    String profile;
+    String content;
+    String images;
+    List<Comment> comments;
+}
