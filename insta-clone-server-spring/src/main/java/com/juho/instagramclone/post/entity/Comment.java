fb90bae03046a27f7d3359cdccc1c964237b4f83
Juho Kim <juho0719@naver.com>
2019-07-09 10:18:09 +0900

    entity updated

--- insta-clone-server-spring/src/main/java/com/juho/instagramclone/post/entity/Comment.java
+++ insta-clone-server-spring/src/main/java/com/juho/instagramclone/post/entity/Comment.java
@@ -1,6 +1,23 @@
 package com.juho.instagramclone.post.entity;
 
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Slf4j
+@Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @NotNull
+    @Column
     String writer;
+
+    @NotNull
+    @Column
     String content;
 }
--- insta-clone-server-spring/src/main/java/com/juho/instagramclone/post/entity/Post.java
+++ insta-clone-server-spring/src/main/java/com/juho/instagramclone/post/entity/Post.java
@@ -1,12 +1,31 @@
 package com.juho.instagramclone.post.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
+@Entity
 public class Post {
 
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @NotNull
+    @Column
     String writer;
+
+    @NotNull
+    @Column
     String profile;
+
+    @NotNull
+    @Column
     String content;
+
+    @Column
     String images;
+
+    @OneToMany
     List<Comment> comments;
 }
